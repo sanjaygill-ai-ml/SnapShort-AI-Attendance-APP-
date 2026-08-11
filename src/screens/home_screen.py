4f0574815c4d6ac3c014b7b1189c0ab32a741876
Sanjay <sanjaygill1410@gmail.com>
2026-08-11 16:01:54 +0530

    Update home_screen.py

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -2,6 +2,7 @@ import streamlit as st
 from src.components.header import header_home
 from src.components.footer import footer_home
 from src.ui.base_layout import style_base_layout, style_background_home
+
 def home_screen():
 
 
@@ -13,14 +14,14 @@ def home_screen():
     col1, col2 = st.columns(2, gap="large")
 
     with col1:
-        st.header("I'm Student")
+        st.markdown("<h2 style='color:black;'>I'm Student</h2>", unsafe_allow_html=True)
         st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
         if st.button('Student Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
             st.session_state['login_type']='student'
             st.rerun()
 
     with col2:
-        st.header("I'm Teacher")
+        st.markdown("<h2 style='color:black;'>I'm Teacher</h2>", unsafe_allow_html=True)
         st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
         if st.button('Teacher Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
             st.session_state['login_type']='teacher'
