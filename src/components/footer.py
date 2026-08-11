e5f646a7b8f31953bc6e046d9f699d13927a4161
Sanjay <sanjaygill1410@gmail.com>
2026-08-11 16:21:45 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,25 +1,20 @@
 import streamlit as st
 
-
 def footer_home():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
-    
     st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
+        <div style="margin-top:1rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created ❤️ by </p>
+        <p style="font-weight:bold; color:black;"> Sanjay Gill </p>
         </div>
-                
-                """, unsafe_allow_html=True)
-
+        """, unsafe_allow_html=True)
 
 def footer_dashboard():
     logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
+        <p style="font-weight:bold; color:black;"> Created ❤️ by </p>
+        <p style="font-weight:bold; color:black;"> Sanjay Gill </p>
         </div>
                 
                 """, unsafe_allow_html=True)
